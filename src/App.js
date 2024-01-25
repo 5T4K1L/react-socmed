b6c0710a026cba4383f5762fe198ba338ed987c1
unknown <akilabiel3@gmail.com>
2024-01-25 20:51:06 +0800

    added testing loaderse

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import "./styles/Global.css";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </BrowserRouter>
+      {isLoaded ? (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      ) : (
+        <PulseLoader color="#36d7b7" />
+      )}
     </>
   );
 }
