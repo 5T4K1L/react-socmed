92c1327f37e4d0968d1cd5579cfc3a2ee9ffdca8
unknown <akilabiel3@gmail.com>
2024-01-27 19:17:32 +0800

    added some features

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import pic from "../images/Tamano.jpg";
 import dummy from "../images/dummy.jpg";
+import like from "../images/heart.png";
+import liked from "../images/heart_liked.png";
+import comment from "../images/comment.png";
+import favorite from "../images/favorite.png";
+import fave from "../images/fave.png";
 
 const Post = () => {
+  const [heart, setHeart] = useState(false);
+  const [faved, setFaved] = useState(false);
+
+  const handleHeart = () => {
+    setHeart((prevHeart) => !prevHeart);
+  };
+
+  const handleFave = () => {
+    setFaved((prevFaved) => !prevFaved);
+  };
+
   return (
     <div className="postContainer">
       <div className="post">
@@ -15,6 +31,31 @@ const Post = () => {
         </div>
         <div className="middle">
           <img src={dummy} alt="" />
+        </div>
+        <div className="bottom">
+          <div className="images">
+            {heart ? (
+              <button onClick={handleHeart}>
+                <img src={liked} alt="" />
+              </button>
+            ) : (
+              <button onClick={handleHeart}>
+                <img src={like} alt="" />
+              </button>
+            )}
+            <button>
+              <img src={comment} alt="" />
+            </button>
+            {faved ? (
+              <button onClick={handleFave}>
+                <img src={fave} alt="" />
+              </button>
+            ) : (
+              <button onClick={handleFave}>
+                <img src={favorite} alt="" />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Homepage.css";
 import pic from "../images/Tamano.jpg";
 import Post from "../components/Post";
+import homepage from "../images/home.png";
+import search from "../images/search.png";
 
 const Homepage = () => {
+  const [profileClicked, setProfileClicked] = useState(false);
+
+  const handleProfile = () => {
+    setProfileClicked((prevProfile) => !prevProfile);
+    setTimeout(() => {
+      setProfileClicked((prevProfile) => !prevProfile);
+    }, 3000);
+  };
+
   return (
     <div className="homepageContainer">
-      <div className="profile">
-        <img src={pic} alt="" />
-      </div>
-      <Post />
+      {profileClicked ? (
+        <div className="profileClicked">
+          <div className="darkenBackground"></div>
+          <div className="container">
+            <div className="signout">
+              <button>Signout</button>
+            </div>
+            <div className="change-account">
+              <button>Change Account</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="profile">
+            <img
+              style={{ cursor: "pointer" }}
+              onClick={handleProfile}
+              src={pic}
+              alt=""
+            />
+          </div>
+          <Post />
+          <div className="footer">
+            <div className="images">
+              <button>
+                <img src={homepage} alt="" />
+              </button>
+              <button>
+                <img src={search} alt="" />
+              </button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
